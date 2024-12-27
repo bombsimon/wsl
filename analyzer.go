b3eb50bfa3119098ddb040df531a195b9a865113
Ludovic Fernandez <ldez@users.noreply.github.com>
2024-12-27 13:23:27 +0100

    test: fix cgo tests (#159)

--- analyzer.go
+++ analyzer.go
@@ -79,12 +79,17 @@ func (wa *wslAnalyzer) flags() flag.FlagSet {
 
 func (wa *wslAnalyzer) run(pass *analysis.Pass) (interface{}, error) {
 	for _, file := range pass.Files {
-		if !wa.config.IncludeGenerated && ast.IsGenerated(file) {
+		filename := getFilename(pass.Fset, file)
+		if !strings.HasSuffix(filename, ".go") {
 			continue
 		}
 
-		filename := getFilename(pass.Fset, file)
-		if !strings.HasSuffix(filename, ".go") {
+		// if the file is related to cgo the filename of the unadjusted position is a not a '.go' file.
+		fn := pass.Fset.PositionFor(file.Pos(), false).Filename
+
+		// The file is skipped if the "unadjusted" file is a Go file, and it's a generated file (ex: "_test.go" file).
+		// The other non-Go files are skipped by the first 'if' with the adjusted position.
+		if !wa.config.IncludeGenerated && ast.IsGenerated(file) && strings.HasSuffix(fn, ".go") {
 			continue
 		}
 
