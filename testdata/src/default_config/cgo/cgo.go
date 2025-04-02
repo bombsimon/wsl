6bab1f5f14ece70bed19fec0bd51a087f4158f0a
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-04-02 23:42:07 +0200

    fix: ignore cgo files

--- testdata/src/default_config/cgo/cgo.go
+++ testdata/src/default_config/cgo/cgo.go
@@ -22,16 +22,16 @@ func _() {
 
 func Advanced() {
 	var foo = 1
-	var bar = 2 // want "declarations should never be cuddled"
-	var biz int // want "declarations should never be cuddled"
+	var bar = 2
+	var biz int
 
 	x := []string{}
 	x = append(x, "literal")
 	notUsed := "just assigning, don't mind me"
-	x = append(x, "not z..") // want "append only allowed to cuddle with appended value"
+	x = append(x, "not z..")
 	useMe := "right away"
 	alsoNotUsed := ":("
-	x = append(x, "just noise") // want "append only allowed to cuddle with appended value"
+	x = append(x, "just noise")
 	x = append(x, useMe)
 
 	_ = foo
