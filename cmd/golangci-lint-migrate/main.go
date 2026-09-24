2c4f010f51703e84305596b88a9620f43b3dfb2d
Simon Sawert <simon@sawert.se>
2026-09-25 00:14:36 +0200

    chore: disable false positive lint

--- cmd/golangci-lint-migrate/main.go
+++ cmd/golangci-lint-migrate/main.go
@@ -159,7 +159,7 @@ func main() {
 }
 
 func getWslConfig(filename string) *WSL {
-	yamlFile, err := os.ReadFile(filepath.Clean(filename))
+	yamlFile, err := os.ReadFile(filepath.Clean(filename)) //nolint:gosec // Reading a user-supplied path is intended.
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
